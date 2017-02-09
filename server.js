4c589d1314581a4fb73101f494124c2d0c3a848e
Paul Jones <paulvjones@gmail.com>
2017-02-09 11:24:51 -0700

    added some notes

--- server.js
+++ server.js
@@ -2,56 +2,58 @@ const express = require('express'),
       session = require('express-session'),
       bodyParser = require('body-parser'),
       massive = require('massive'),
-      passport = require('passport'),
-      Auth0Strategy = require('passport-auth0'),
+      passport = require('passport'),  // this
+      Auth0Strategy = require('passport-auth0'), // and this are required for auth0 to work.
       config = require('./config.js'),
       cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+// ALWAYS INIT IN THIS ORDER!!!!!
 app.use(session({
   resave: true, //Without this you get a constant warning about default values
   saveUninitialized: true, //Without this you get a constant warning about default values
-  secret: 'keyboardcat'
+  secret: 'keyboardcat' // normally move this into its own config file
 }))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize()); //connects passport into express
+app.use(passport.session()); //this tells passport into express-sessions (allows us to use req.user in endpoints ie. use req.user.id to fetch friends, comments, etc from database)
 
 app.use(express.static('./public'));
 
 
 
-/////////////
+//////////////
 // DATABASE //
-/////////////
+//////////////
 const massiveInstance = massive.connectSync({connectionString: 'postgres://localhost/sandbox'})
 
 app.set('db', massiveInstance);
 const db = app.get('db');
 
+//SAMPLE database call
 // db.create_user(function(err, user) {
 //   if (err) console.log(err);
 //   else console.log('CREATED USER');
 //   console.log(user);
 // })
 
-
+// MUST HAVE THESE EXACT PROPERTIES (case-sensitive)
 passport.use(new Auth0Strategy({
    domain:       config.auth0.domain,
    clientID:     config.auth0.clientID,
    clientSecret: config.auth0.clientSecret,
    callbackURL:  '/auth/callback'
-  },
+  }, //now we give auth0 a function -- it must be exactly like this!
   function(accessToken, refreshToken, extraParams, profile, done) {
     //Find user in database
-    db.getUserByAuthId([profile.id], function(err, user) {
+    db.getUserByAuthId([profile.id], function(err, user) { //this is the callback function to do after return from Auth0
       user = user[0];
       if (!user) { //if there isn't one, we'll create one!
         console.log('CREATING USER');
         db.createUserByAuth([profile.displayName, profile.id], function(err, user) {
           console.log('USER CREATED', userA);
-          return done(err, user[0]); // GOES TO SERIALIZE USER
+          return done(err, user[0]); // GOES TO SERIALIZE USER **done function  is the same thing as 'next' function
         })
       } else { //when we find the user, return it
         console.log('FOUND USER', user);
@@ -80,7 +82,7 @@ passport.deserializeUser(function(userB, done) {
 
 
 
-app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth', passport.authenticate('auth0')); // here's our endpoint that will hit auth0
 
 
 //**************************//
@@ -91,7 +93,7 @@ app.get('/auth', passport.authenticate('auth0'));
 //   res.redirect("/");
 // });
 
-app.get('/auth/callback',
+app.get('/auth/callback', //this redirects on success -- we can add other callbacks here
   passport.authenticate('auth0', {successRedirect: '/'}), function(req, res) {
     res.status(200).send(req.user);
 })
